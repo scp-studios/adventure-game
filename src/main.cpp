d346b260842ca4d601b7256d0a4dc13fb2794de2
can pooper <domesticatedhuman9999@gmail.com>
2022-02-02 10:41:32 -0500

    git push

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 
 int main(){
     char teaChoice;
-    std::cout << "Hello!\nWelcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea? [y/n] ";
+    std::cout << "# Hello!\n# Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea? [y/n] ";
     std::cin >> teaChoice;
     switch (teaChoice)
     {
     case 'y':
-        std::cout << "Oops! You got poisoned by Shiva Jay.\nYou died.\n";
+        std::cout << "# Oops! You got poisoned by Shiva Jay.\nYou died.\n";
         return 0;
     case 'n':
         break;
     default:
-        std::cout << "Hey listen here, '" << teaChoice << "' isn't a valid choice.\n";
+        std::cout << "# Hey listen here, '" << teaChoice << "' isn't a valid choice.\n";
         return 0;
     }
     return 0;
