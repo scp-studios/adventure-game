0dff084840eabcff6197a79acc0bd75473f5c830
can pooper <domesticatedhuman9999@gmail.com>
2022-02-02 11:30:28 -0500

    d

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 void mountainHike()
 {
     std::cout << R"NengTheProtagon(
-"So, you don't like tea, eh?" said Shiva Jay.
-#"No, not really", you replied, "I'm not exactly a fan of tea. 
-#"Very well, then. You into hiking?"
-#"Well, not really. I've hiked before, but not much."
-#"Okay. So, would you like to hike up this mountain?"
+# "So, you don't like tea, eh?" said Shiva Jay.
+# "No, not really", you replied, "I'm not exactly a fan of tea. 
+# "Very well, then. You into hiking?"
+# "Well, not really. I've hiked before, but not much."
+# "Okay. So, would you like to hike up this mountain?"
     
     Choices:
     [1]: Hike up the mountain with Shiva Jay.
@@ -26,7 +26,7 @@ void mountainHike()
 int main()
 {
     char teaChoice;
-    std::cout << "# Hello!\n# \"Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea?\" [y/n] ";
+    std::cout << "# \"Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea?\" [y/n] ";
     std::cin >> teaChoice;
     switch (teaChoice)
     {
