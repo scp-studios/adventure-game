3e2056ebb0623516a5934217aba4248d2d5c960f
can pooper <domesticatedhuman9999@gmail.com>
2022-02-02 11:35:39 -0500

    firmatug

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 
 void mountainHike()
 {
-    std::cout << R"NengTheProtagon("So, you don't like tea, eh?" said Shiva Jay.
-"No, not really", you replied, "I'm not exactly a fan of tea. 
-"Very well, then. You into hiking?"
-"Well, not really. I've hiked before, but not much."
-"Okay. So, would you like to hike up this mountain?"
+    std::cout << R"NengTheProtagon(
+# "So, you don't like tea, eh?" said Shiva Jay.
+# "No, not really", you replied, "I'm not exactly a fan of tea. 
+# "Very well, then. You into hiking?"
+# "Well, not really. I've hiked before, but not much."
+# "Okay. So, would you like to hike up this mountain?"
 
 Choices:
 [1]: Hike up the mountain with Shiva Jay.
@@ -17,22 +18,6 @@ Choices:
 [*]: Stand still and do nothing.
 
 Your choice: )NengTheProtagon";
-    std::cout << R"NengTheProtagon(
-# "So, you don't like tea, eh?" said Shiva Jay.
-# "No, not really", you replied, "I'm not exactly a fan of tea. 
-# "Very well, then. You into hiking?"
-# "Well, not really. I've hiked before, but not much."
-# "Okay. So, would you like to hike up this mountain?"
-    
-    Choices:
-    [1]: Hike up the mountain with Shiva Jay.
-    [2]: Say no to hiking up the mountain with Shiva Jay.
-    [3]: Start running away from Shiva Jay.
-    [4]: Dial 911.
-    [*]: Stand still and do nothing.
-    
-    Your choice: )NengTheProtagon";
-    
     char option;
     std::cin >> option;
 }
@@ -40,7 +25,7 @@ Your choice: )NengTheProtagon";
 int main()
 {
     char teaChoice;
-    std::cout << "# \"Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea?\" [y/n] ";
+    std::cout << "# \"Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea?\" \n\nChoices:\n[y]: Accept the tea.\n[n]: Decline the tea and make Shiva Jay sad.\n\nYour choice: ";
     std::cin >> teaChoice;
     switch (teaChoice)
     {
