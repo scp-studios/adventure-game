4076d6d52da7cec4c23ad6dc139203f835060b13
can pooper <domesticatedhuman9999@gmail.com>
2022-02-02 10:36:44 -0500

    add tea

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@ int main(){
     switch (teaChoice)
     {
     case 'y':
-        std::cout << "Oops! You got poisoned by Shiva Jay.\n";
+        std::cout << "Oops! You got poisoned by Shiva Jay.\nYou died.\n";
         return 0;
     case 'n':
-    default:
         break;
+    default:
+        std::cout << "Hey listen here, '" << teaChoice << "' isn't a valid choice.\n"
+        return 0;
     }
     return 0;
 }
