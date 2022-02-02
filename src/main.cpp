bc978371c20fd120b9e6bfc612c0702c1efdf45b
Hello56721 <Hello56721@github.com>
2022-02-02 15:26:54 -0500

    Moved the messages to a header file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <string>
 
-void deathByGunshotOnMountain()
+#include "messages.hpp"
+
+static void mountainHike();
+
+static void deathByGunshotOnMountain();
+
+static void gettingChasedByShivaJay();
+
+static void uncertainPlot1();
+
+int main()
 {
+    char teaChoice;
+    std::cout << startingMessageOne;
     
+    std::string nullLine;
+    std::getline(std::cin, nullLine);
+
+    std::cout << startingMessageTwo;
+    
+    std::cin >> teaChoice;
+    switch (teaChoice)
+    {
+    case 'y':
+        std::cout << startingDeathMessageOne;
+        return 0;
+    case 'n':
+        mountainHike();
+        break;
+    default:
+        std::cout << "# Hey listen here, '" << teaChoice << "' isn't a valid choice.\n";
+        return 0;
+    }
+    return 0;
 }
 
-void uncertainPlot1()
+static void mountainHike()
 {
-    
-}
-
-void gettingChasedByShivaJay()
-{
-    
-}
-
-void mountainHike()
-{
-    std::cout << R"NengTheProtagon(
-# "So, you don't like tea, eh?" said Shiva Jay.
-# "No, not really", you replied, "I'm not exactly a fan of tea. 
-# "Very well, then. You into hiking?"
-# "Well, not really. I've hiked before, but not much."
-# "Okay. So, would you like to hike up this mountain?"
-
-Choices:
-[1]: Hike up the mountain with Shiva Jay.
-[2]: Say no to hiking up the mountain with Shiva Jay.
-[3]: Start running away from Shiva Jay.
-[4]: Dial 911.
-[*]: Stand still and do nothing.
-
-Your choice: )NengTheProtagon";
+    std::cout << mountainHikeMainMessage;
 
     char option;
     std::cin >> option;
@@ -55,22 +62,17 @@ Your choice: )NengTheProtagon";
     }
 }
 
-int main()
+static void deathByGunshotOnMountain()
 {
-    char teaChoice;
-    std::cout << "# \"Welcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea?\" \n\nChoices:\n[y]: Accept the tea.\n[n]: Decline the tea and make Shiva Jay sad.\n\nYour choice: ";
-    std::cin >> teaChoice;
-    switch (teaChoice)
-    {
-    case 'y':
-        std::cout << "# Oops! You got poisoned by Shiva Jay.\nYou died.\n";
-        return 0;
-    case 'n':
-        mountainHike();
-        break;
-    default:
-        std::cout << "# Hey listen here, '" << teaChoice << "' isn't a valid choice.\n";
-        return 0;
-    }
-    return 0;
+    
+}
+
+static void uncertainPlot1()
+{
+    
+}
+
+static void gettingChasedByShivaJay()
+{
+    
 }
