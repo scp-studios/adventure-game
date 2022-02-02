3ba96e8208b20e6c4389a149e23429e144f4a1c5
Hello56721 <Hello56721@github.com>
2022-02-02 17:09:43 -0500

    Changed the comment a little.

--- src/main.hpp
+++ src/main.hpp
@@ -2,7 +2,7 @@
 #define B6A35143_AA7E_4EB0_9133_0958CDC8765D
 
 // Do not confuse this with a main header. This simply is used to contain the
-// main branch and it's direct children.
+// the direct children of the main branch.
 
 namespace scp
 {
