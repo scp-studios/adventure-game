f8b5be54b746309a52813c77d267201d6c41ded7
can pooper <domesticatedhuman9999@gmail.com>
2022-02-02 10:32:09 -0500

    add start

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello!" << std::endl;
+int main(){
+    char teaChoice;
+    std::cout << "Hello!\nWelcome to this adventure game, jolly fellow. I am Shiva Jay, your guide. Before we start, would you care for a spot of tea? [y/n]\n";
+    std::cin >> teaChoice;
+    switch (teaChoice)
+    {
+    case "y":
+        std::cout << "Oops! You got poisoned by Shiva Jay.\n";
+        return 0;
+    case "n":
+    default:
+        break;
+    }
     return 0;
 }
